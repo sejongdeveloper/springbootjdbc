7f952fc2cf63f48737af24ae6455720082932a8a
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 15:39:03 +0900

    스프링 데이터 2부: 인메모리 데이터베이스

---
+++ src/main/java/me/whiteship/springbootjdbc/H2Runner.java
@@ -0,0 +1,37 @@
+package me.whiteship.springbootjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            String url = connection.getMetaData().getURL();
+            System.out.println("url = " + url);
+
+            String userName = connection.getMetaData().getUserName();
+            System.out.println("userName = " + userName);
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+            statement.execute(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'keesun')");
+    }
+}
